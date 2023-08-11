28a4563deb5b2cbff6c4f07e1214099ee78122be
남형석 <namhyeongseog@azerc-m2-MacBook-Air.local>
2023-08-11 12:20:18 +0900

    1. add login 2. add signUp

---
+++ mongoDB.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(
+    "mongodb://admin:1234@svc.sel3.cloudtype.app:31947/?authMechanism=DEFAULT",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
+
+const UserSchema = new mongoose.Schema({
+  name: String,
+  age: Number,
+});
+const UserModel = mongoose.model("users", UserSchema);
+
+const LoginSchema = new mongoose.Schema({
+  id: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+});
+const LoginModelCollecton = mongoose.model("signup", LoginSchema);
+
+module.exports = { UserModel, LoginModelCollecton };
